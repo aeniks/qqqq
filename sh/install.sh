1942a10a8d75ec2618653dde60bb746351c597b6
bobo krull <163834223+bobokrull@users.noreply.github.com>
2024-03-18 10:35:06 +0100

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -2,9 +2,26 @@
 ###########
 echo -ne "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
 \t -- Welcome to Installer X ";
-read -re "ok"; if [ $ok -eq y }; then
-echo ok;
-mkdir /ants -m 775; cd /;
-git clone https://github.com/bobokrull/ants||apt install -yqq git 2>x && git clone https://github.com/bobokrull/ants
-fi
-cd /ants/; echo -ne "\n\n\n\n\n\n\n\n"
+pro() {
+alias tf='tput setaf $((RANDOM%16));'
+alias tb='tput setab $((RANDOM%16));'
+c2="$cyan --$re"; tput civis;
+$1 $2 $3 $4 $pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2; PROC_ID=$!; while kill -0 "$PROC_ID"&>/dev/null; 
+do for X in "[        ]" "[$(tf)=$re       ]" "[$(tf)==$re      ]" "[$(tf)===$re     ]" "[$(tf)====    $re]"  "[ $(tf)====   $re]" \
+"[  $(tf)====$re  ]" "[   $(tf)==== $re]" "[    $(tf)====$re]" "[     "$(tf)"===$re]" "[      "$(tf)"=="$re"]" "[       =]" "[        ]" "[        ]" "[        ]"; 
+do echo -e "  [$(tb)  $re]$c2 Executing $rev $1 $2 $3 $4 $pro $re"$c2" $X"; tput cuu1; sleep 0.08; done; done;
+echo -e "\t\t\t\t\t\t [  "$green"DONE"$re"  ] \n\n\n\n\n"; tput sgr0 cnorm;
+}
+yno() {
+if [ -z "$1" ]; then echo -e "\n\t $c2 Try$dim ["$re"yno question? command 1"$dim"]$re and use quotes...\n"; fi; 
+echo -e "\n\n\t $re$c2 $1 $white$dim["$re$bold"Y$dim/"$re$bold"n$dim]$re $(tput sc)\n\n\n\n"; tput rc cuu 5; read -n1 yn; 
+if [ "$yn" == "${yn#[Nn]}" ]; then echo -en "\t $c2 OK"; pro $2; else echo "nope"; fi; tputsgr0;
+}
+##
+##
+cd /;
+yno "install ants?" "git clone https://github.com/bobokrull/ants"
+echo ok; cd /ants/; echo -ne "\n\n\n\n\n\n\n\n";
+#git clone https://github.com/bobokrull/ants||apt install -yqq git 2>x && git clone https://github.com/bobokrull/ants
+###########
+## show loaded state
