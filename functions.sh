c036f7e946a4afd226eb07abb8782b98b6446a85
aeniks <hello@aeniks.com>
2024-04-13 09:49:14 +0200

    xxxx
    axax

--- functions.sh
+++ functions.sh
@@ -128,7 +128,7 @@ alias tf='tput setaf $((RANDOM%16));'
 alias tb='tput setab $((RANDOM%16));'
 c2="$cyan --$re"; tput civis;
 $1 $2 $3 $4 $pro &>/dev/null & disown; tput cuu 8; tput ed; tput cud 2; PROC_ID=$!; while kill -0 "$PROC_ID"&>/dev/null; 
-do for X in "[        ]" "[$(tf)=$re       ]" "[$(tf)==$re      ]" "[$(tf)===$re     ]" "[$(tf)====    $re]"  "[ $(tf)====   $re]" \
+do for X in "[        ]" "[$(tf)=$re       ]" "[$(tf)==$re      ]" "[$(tf)===$re     ]" "[$(tf)====    $re]"  "[ $(tf)====   $re]" \
 "[  $(tf)====$re  ]" "[   $(tf)==== $re]" "[    $(tf)====$re]" "[     "$(tf)"===$re]" "[      "$(tf)"=="$re"]" "[       =]" "[        ]" "[        ]" "[        ]"; 
 do echo -e "  [$(tb)  $re]$c2 Executing $rev $1 $2$3$4$pro $re"$c2" $X"; tput cuu1; sleep 0.08; done; done;
 echo -e "\t\t\t\t\t\t [  "$green"DONE"$re"  ] \n\n\n\n\n"; tput cnorm;
@@ -142,3 +142,8 @@ echo -e "\n\n\t $re$c2 $1 $white$dim["$re$bold"Y$dim/"$re$bold"n$dim]$re $(tput 
 if [ "$yn" == "${yn#[Nn]}" ]; then echo -en "\t $c2 OK"; pro $2; else echo "nope"; fi;  
 }
 
+
+screensaver() {
+clear;
+for i in $(seq 222); do tput civis; tput cup $((RANDOM%$444)); fortune|qqqq|pr --omit-header --indent="$((RANDOM%222))"|lolcat -s 222 -p 666 -a; sleep 4; tput rc cnorm; done; 
+}
