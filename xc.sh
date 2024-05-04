40c2e1c4b4755e583a68ad1cd7485df035527eba
aeniks <hello@aeniks.com>
2024-05-04 13:22:35 +0200

    added /gh/qqqq/xc.sh

---
+++ xc.sh
@@ -0,0 +1,4 @@
+
+tput indn $LINES; tput cuu $LINES; echo -e "\n\t Hello"; 
+echo -e "\n\n\t\t $blink ¯\(ツ)/¯$re "; echo -e " \n\n $ll This script should be run as root... [ sudo -s ] "; read;
+###########
